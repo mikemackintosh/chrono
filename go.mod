713e22e8568bf0e5246f5f0d1c8d403d041f24b7
Mike Mackintosh <m@zyp.io>
2021-07-31 17:04:15 -0700

    update code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mikemackintosh/chrono
 
 go 1.16
+
+require golang.org/dl v0.0.0-20210713194856-38ddc79c2163 // indirect
--- main.go
+++ main.go
@@ -3,45 +3,82 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
-var flagMilli bool
-
-const (
-	ENV_CHRONO     = "CHRONO_TIMESTAMP"
-	ENV_CHRONO_DUR = "CHRONO_DUR"
+var (
+	flagMilli   bool
+	flagVersion bool
 )
 
 func init() {
-	flag.BoolVar(&flagMilli, "m", false, "Show only current milliseconds")
+	flag.BoolVar(&flagMilli, "m", false, "Show current milliseconds")
+	flag.BoolVar(&flagVersion, "v", false, "Show version")
 }
 
 func main() {
+	var duration time.Duration
+
 	flag.Parse()
 
-	var t = time.Now()
-	var startTime = os.Getenv(ENV_CHRONO)
+	if flagVersion {
+		fmt.Printf("1.0")
+		os.Exit(0)
+	}
 
+	var t = time.Now().UnixMilli()
 	if flagMilli {
-		fmt.Println(t.UnixNano() / 1000000)
+		fmt.Println(t)
 		os.Exit(0)
 	}
 
+	var startTime = os.Getenv("START_TIME")
 	if len(startTime) == 0 {
-		fmt.Printf("unset %s;\n", ENV_CHRONO_DUR)
-		fmt.Printf("export %s=%v;\n", ENV_CHRONO, t.UnixNano())
+		fmt.Printf("0ms")
 		os.Exit(0)
 	}
-
-	var duration time.Duration
 	i, _ := strconv.Atoi(startTime)
+
 	if i > 0 {
-		duration = time.Since(time.Unix(0, int64(i)))
+		duration = time.Since(time.UnixMilli(int64(i)))
 	}
 
-	fmt.Printf("export %s=%v;\n", ENV_CHRONO_DUR, duration)
-	fmt.Printf("unset %s;\n", ENV_CHRONO)
+	fmt.Printf("%s", humanizeDuration(duration))
+}
+
+// https://gist.github.com/harshavardhana/327e0577c4fed9211f65
+func humanizeDuration(duration time.Duration) string {
+	days := int64(duration.Hours() / 24)
+	hours := int64(math.Mod(duration.Hours(), 24))
+	minutes := int64(math.Mod(duration.Minutes(), 60))
+	seconds := int64(math.Mod(duration.Seconds(), 60))
+	milliseconds := int64(math.Mod(float64(duration.Milliseconds()), 1000))
+
+	chunks := []struct {
+		singularName string
+		amount       int64
+	}{
+		{"d", days},
+		{"h", hours},
+		{"m", minutes},
+		{"s", seconds},
+		{"ms", milliseconds},
+	}
+
+	parts := []string{}
+
+	for _, chunk := range chunks {
+		switch chunk.amount {
+		case 0:
+			continue
+		default:
+			parts = append(parts, fmt.Sprintf("%d%s", chunk.amount, chunk.singularName))
+		}
+	}
+
+	return strings.Join(parts, " ")
 }
